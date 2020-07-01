1be7f6ee138af5fcd56460727fb701741101aaec
Joshua Go <38119570+jgo28@users.noreply.github.com>
2020-07-01 16:36:02 -0700

    First update (#1)
    * readme update
    
    * more database communication functions; script to test functions
    
    * fixed spelling error with requirement
    
    * more user tools to manipulate the database
    
    * retrieve user data

---
+++ script.py
@@ -0,0 +1,31 @@
+import utils.main as m
+
+'''
+Adding new users
+'''
+# m.add_user("921010", "aly", 0, 0, 0)
+# m.add_user("921010", "jake", 0, 0, 0)
+# m.add_user("9211011", "sda", 0, 0, 0)
+# m.add_user("92102343", "asw", 0, 0, 0)
+# m.add_user("76565623", "wyh", 0, 0, 0)
+
+'''
+Retrieving user data
+'''
+# print(m.get_all_users())
+# print(m.get_user_count())
+user = m.get_user("9211011")
+print(
+    f"{user.name}, {user.discord_id}, {user.level}, "
+    f"{user.experience}, {user.message_count}"
+)
+
+'''
+Update user information
+'''
+# m.update_user("76565623", name="notjake")
+# m.update_user("76565623", lvl=2)
+# m.update_user("76565623", exp=200)
+# m.update_user("76565623", msg_count=40)
+# m.update_user("dfg", lvl=4, msg_count=430)
+# m.update_user("921010", lvl=16, msg_count=2345, exp=35789, name="notaly")
