6d7e40110e5f7931c4725be4b447ca8204a20b77
Joshua Go <38119570+jgo28@users.noreply.github.com>
2020-07-02 12:49:50 -0700

    Development (#3)
    * readme update
    
    * more database communication functions; script to test functions
    
    * fixed spelling error with requirement
    
    * more user tools to manipulate the database
    
    * retrieve user data
    
    * delete_user added
    
    * Modularization of sqlalchemy (#2)
    
    * main.py -> database.py
    
    * broke database commands into modules
    
    * more modular sqlalchemy commands
    
    * userinfo more functions to modify attributes

---
+++ utils/database/models.py
@@ -0,0 +1,20 @@
+import sqlalchemy as db
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class Users(Base):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    discord_id = db.Column(db.String(length=50))
+    name = db.Column(db.String(length=50))
+    level = db.Column(db.Integer)
+    experience = db.Column(db.Integer)
+    message_count = db.Column(db.Integer)
+
+    # Allows us to print out a User object nicely
+    def __repr__(self):
+        text = ("<User(name='{self.name}', discord_id='{self.discord_id}',"
+                "level='{self.level}', experience='{self.experience}',"
+                "message_count='{self.message_count}')")
+        return text
