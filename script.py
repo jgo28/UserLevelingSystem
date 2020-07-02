dbd9c1559d0c9e68615d44e635964fd02780d5c8
Joshua Go <38119570+jgo28@users.noreply.github.com>
2020-07-02 12:40:32 -0700

    Modularization of sqlalchemy (#2)
    * main.py -> database.py
    
    * broke database commands into modules
    
    * more modular sqlalchemy commands
    
    * userinfo more functions to modify attributes

--- script.py
+++ script.py
@@ -1,33 +1,40 @@
-import utils.main as m
+import utils.database.methods as methods
+import utils.database.models as model
+import utils.database.config as i
+
+metadata = i.get_metadata()
+engine = i.get_engine()
+connection = i.get_connection()
+db = methods.Methods(engine, metadata, connection)
 
 '''
 Adding new users
 '''
-# m.add_user("921010", "aly", 0, 0, 0)
-# m.add_user("921010", "jake", 0, 0, 0)
-# m.add_user("9211011", "sda", 0, 0, 0)
-# m.add_user("92102343", "asw", 0, 0, 0)
-# m.add_user("76565623", "wyh", 0, 0, 0)
+db.add_user("921010", "aly", 0, 0, 0)
+db.add_user("921010", "jake", 0, 0, 0)
+db.add_user("9211011", "sda", 0, 0, 0)
+db.add_user("92102343", "asw", 0, 0, 0)
+db.add_user("76565623", "wyh", 0, 0, 0)
 
 '''
 Retrieving user data
 '''
-# print(m.get_all_users())
-# print(m.get_user_count())
-user = m.get_user("9211011")
+print(db.get_all_users())
+print(db.get_user_count())
+user = db.get_user("9211011")
 print(user)
 
 '''
 Update user information
 '''
-# m.update_user("76565623", name="notjake")
-# m.update_user("76565623", lvl=2)
-# m.update_user("76565623", exp=200)
-# m.update_user("76565623", msg_count=40)
-# m.update_user("dfg", lvl=4, msg_count=430)
-# m.update_user("921010", lvl=16, msg_count=2345, exp=35789, name="notaly")
+db.update_user("76565623", name="notjake")
+db.update_user("76565623", lvl=2)
+db.update_user("76565623", exp=200)
+db.update_user("76565623", msg_count=40)
+db.update_user("dfg", lvl=4, msg_count=430)
+db.update_user("921010", lvl=16, msg_count=2345, exp=35789, name="notaly")
 
 '''
 Delete user data
 '''
-m.delete_user("921010")
+db.delete_user("921010")
--- utils/user.py
+++ utils/user.py
@@ -1,5 +1,5 @@
 
-class UserDB:
+class UserInfo:
     def __init__(self, name, id, level, exp, msg_count):
         self.user = name
         self.id = id
@@ -21,3 +21,22 @@ class UserDB:
 
     def get_msg_count(self):
         return self.msg_count
+
+    def set_username(self, name)
+        self.user = name
+
+    def set_discord_id(self, id):
+        self.id = id
+
+    def set_level(self, level):
+        self.level = level
+
+    def set_exp(self, exp):
+        self.exp = exp
+
+    def set_msg_count(self, msgs)
+        self.msg_count = msgs
+
+    def calc_exp(self):
+        pass
+
